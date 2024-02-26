91f9d6d5e4fa2efb82ca82bd9bf08eba5302e84d
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2024-02-26 18:24:28 -0500

    small change to footer

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
           maxWidth="1024px"
           height={16}
         >
-          <p>Gary's Sweet Portolio • © 2024</p>
+          <p>Gary's Portolio • © 2024</p>
         </Flex>
       </footer>
     </Box>
